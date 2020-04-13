163f1fe54d941c1e9c70dc636da014e2f675f642
Aleph Melo <alephmelo@icloud.com>
2020-04-13 11:46:43 +0200

    Add interfaces.

--- models/user.go
+++ models/user.go
@@ -10,3 +10,14 @@ var (
 	users  []*User
 	nextID = 1
 )
+
+func getUsers() []*User {
+	return users
+}
+
+func addUser(userObject User) (User, error) {
+	userObject.ID = nextID
+	nextID++
+	users = append(users, &userObject)
+	return userObject, nil
+}
