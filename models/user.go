fcf2e17dbebf8b8335e69b9379ee79c51e68931f
Aleph Melo <alephmelo@icloud.com>
2020-04-16 11:11:02 +0200

    Finish controllers.

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID        int
 	FirstName string
@@ -11,13 +16,46 @@ var (
 	nextID = 1
 )
 
-func getUsers() []*User {
+func GetUsers() []*User {
 	return users
 }
 
-func addUser(userObject User) (User, error) {
+func AddUser(userObject User) (User, error) {
+	if userObject.ID != 0 {
+		return User{}, errors.New("user must not include ID")
+	}
 	userObject.ID = nextID
 	nextID++
 	users = append(users, &userObject)
 	return userObject, nil
+}
+
+func GetUserByID(id int) (User, error) {
+	for _, user := range users {
+		if user.ID == id {
+			return *user, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with id %v not found", id)
+}
+
+func UpdateUser(user User) (User, error) {
+	for index, candidate := range users {
+		if candidate.ID == user.ID {
+			users[index] = &user
+			return user, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with id %v not found", user.ID)
+}
+
+func DeleteUserByID(id int) error {
+	for index, candidate := range users {
+		if candidate.ID == id {
+			users = append(users[:index], users[:index+1]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with id %v not found", id)
 }
